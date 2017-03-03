d6d63be44779086ce1124cad0bd30cc718a7b07a
Kris Wilson <kwilson@twitter.com>
2017-03-02 22:38:40 -0800

    Longer runtime.

--- cffi.py
+++ cffi.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+import time
+
 from cffi import FFI
 
-ffi = FFI()
-ffi.cdef('int double(int);')
-lib = ffi.dlopen('rust_lib/target/debug/libtest.dylib')
-print([lib.double(x) for x in xrange(501)])
+
+def main():
+  ffi = FFI()
+  ffi.cdef('int double(int);')
+  lib = ffi.dlopen('rust_lib/target/debug/libtest.dylib')
+  for x in xrange(1, 11):
+    nums = [lib.double(y) * x for y in xrange(501)]
+    print(nums)
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+  main()
